4893dcb86530b597c8b35d7d74dd95a55d74e3b6
Mutuma Gitonga <mgitonga150@gmail.com>
2022-08-25 17:33:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+function submitData (userName, userEmail) {
+  const userData = {
+    name: userName,
+    email: userEmail
+  }
+
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(userData)
+  })
+  .then(response => response.json())
+  .then(user => {
+    const p = document.createElement('p');
+    p.textContent = user.id;
+
+    document.querySelector('body').appendChild(p);
+  })
+  .catch((error) => {
+    const divError = document.createElement('div');
+    divError.textContent = error.message;
+
+    document.querySelector('body').appendChild(divError);
+  })
+}
+
